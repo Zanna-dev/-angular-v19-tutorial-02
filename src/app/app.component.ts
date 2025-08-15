1282504d5bbab76dc848bb3ff2f071fed2898d1d
Zanna-dev <pamn2704@gmail.com>
2025-08-15 12:13:16 +0100

    Install Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { DirectivesComponent } from './directives/directives.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserComponent, DataBindingComponent],
+  imports: [RouterOutlet, 
+    // UserComponent, 
+    // DataBindingComponent
+    DirectivesComponent
+   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
