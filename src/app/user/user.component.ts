dee2fe1347e37667c7a8fc07e50f234278c65d03
Zanna-dev <pamn2704@gmail.com>
2025-08-18 16:02:02 +0100

    Implement Life Cycle Hooks in Angular

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  constructor(private router : Router) {
 
+  }
+
+  loginData(){
+// user verificarion logic
+    // this.router.navigateByUrl("structural-directive");
+    this.router.navigate(['structural-directive']);
+  }
 }
